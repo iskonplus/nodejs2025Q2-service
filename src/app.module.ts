f89676ef153b3252faa9844c7399f154ce75d115
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-25 19:32:40 +0100

    feat: load PORT from .env and configure application startup

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavoritesModule } from './favorites/favorites.module';
 
+
 @Module({
   imports: [
     UserModule,
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
