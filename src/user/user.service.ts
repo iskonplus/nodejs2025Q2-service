78598ce80e7cab3ae551d03f9103e33f822c2611
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-25 20:14:32 +0100

    feat: add get all public users logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { User, PublicUser } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [];
+
+  private stripPassword(user: User): PublicUser {
+    const { password, ...rest } = user;
+    return rest;
+  }
+
+  getAllUsers(): PublicUser[] {
+    return this.users.map((user) => this.stripPassword(user));
+  }
+}
