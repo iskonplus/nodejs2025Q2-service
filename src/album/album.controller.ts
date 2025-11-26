8221d773d864034a23cebf0c5fd592658d0d9aee
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-26 15:27:18 +0100

    feat: add controller & service logic for get album by id

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { AlbumService } from './album.service';
 
 @Controller('album')
-export class AlbumController {}
+export class AlbumController {
+  constructor(readonly albumService: AlbumService) {}
+
+  @Get()
+  getAllAlbums() {
+    return this.albumService.getAllAlbums();
+  }
+
+  @Get(':id')
+  getAlbumById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.albumService.findOne(id);
+  }
+}
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Album } from './album.entity';
+import { httpErrors } from 'src/handleErrors/http-errors';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  private albums: Album[] = [
+    {
+      name: 'The Marshall Mathers LP',
+      year: 2000,
+      artistId: null,
+      id: '5f8c0b3e-0d7a-4e4f-a77f-6cf5e7e3b452',
+    },
+  ];
+
+  getAllAlbums(): Album[] {
+    return this.albums;
+  }
+
+  findOne(id: string): Album {
+    const album = this.albums.find((album) => album.id === id);
+
+    if (!album) throw httpErrors.notFound('Album not found');
+    return album;
+  }
+}
