0a8114db11d23bc8c1981d514dded96c92e0a9a7
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-12-01 19:35:52 +0100

    fead: expose OpenAPI docs at /doc

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config/config';
@@ -12,6 +15,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const openApiPath = join(process.cwd(), 'doc', 'openapi.yaml');
+  const document = YAML.load(openApiPath);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(PORT);
 }
