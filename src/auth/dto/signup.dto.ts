ef6f8a42476a4bf31ac6a27a38e4e9e06f2b5543
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-12-12 12:06:46 +0100

    docs: add request body examples for Try it out

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class SignupDto {
+  @ApiProperty({ example: 'TEST_LOGIN' })
   @IsString()
   login: string;
 
+  @ApiProperty({ example: 'TEST_PASSWORD' })
   @IsString()
   password: string;
 }
